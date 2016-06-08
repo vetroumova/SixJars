68f369395fd4a6af4ba9e77653e0067545df6bc8
vetroumova <o.vitroumova@gmail.com>
2016-06-08 15:35:05 +0300

    Initial commit

---
+++ app/src/androidTest/java/org/itstep/android5/vetroumova/newbeginning/sixjars/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.itstep.android5.vetroumova.newbeginning.sixjars;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
